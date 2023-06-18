20160102f211f6845ad147a6d26985b8b7766164
Stefania <stefania.struzzi@gmail.com>
2023-06-19 00:13:12 +0200

    ultima tentativo per fare no scroll bar. non riu

--- no_scroll_layers.js
+++ no_scroll_layers.js
@@ -1,25 +1,17 @@
-var larghezza = "";
-var altezza = "";
- function Inizializza() {
-  if (document.all) {
-   larghezza = TextScroll.offsetWidth;
-   altezza = TextScroll.offsetHeight;
-   setInterval("TextScroll_IE()", 1);
-   TextScroll.style.visibility = "Visible";
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
-  else if (document.layers) {
-   larghezza = document.TextScroll.document.width;
-   altezza = document.TextScroll.document.height;
-   setInterval("TextScroll_NN()", 1);
-   document.TextScroll.visibility = "Show";
-  }
- }
- function TextScroll_IE() {
-  TextScroll.style.pixelLeft = document.body.scrollLeft + document.body.clientWidth - larghezza - 10;
-  TextScroll.style.pixelTop = document.body.scrollTop + document.body.clientHeight - altezza - 10;
- }
- function TextScroll_NN() {
-  document.TextScroll.left = pageXOffset + window.innerWidth - larghezza - 10;
-  document.TextScroll.top = pageYOffset + window.innerHeight - altezza - 10;
- }
-window.onload = Inizializza;
+}
